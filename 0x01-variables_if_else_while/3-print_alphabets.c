d59c97cfc05350a2207691a3daee30c4d9f838c4
ISHMAEL NYAME <ishmaelnyame7@gmail.com>
2023-06-18 15:28:20 -0700

    PRINT ALL ALPHA

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char myalphabet;
+
+for(myalphabet = 'a'; myalphabet <= 'z'; myalphabet++)
+{
+putchar(myalphabet);
+}
+
+for(myalphabet = 'A'; myalphabet <= 'Z'; myalphabet++)
+{
+putchar(myalphabet);
+
+putchar('\n');
+
+return(0);
+
+}
+}
