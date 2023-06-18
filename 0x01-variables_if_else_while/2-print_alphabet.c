8df18c2d5d395493a5730fc0c8dd83a113bab3c0
ISHMAEL NYAME <ishmaelnyame7@gmail.com>
2023-06-18 12:06:23 -0700

    ALLHABET DISPLAY

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char myalphabet;
+
+for(myalphabet ='a'; myalphabet <='z'; myalphabet++)
+{
+putchar(myalphabet);
+}
+putchar('\n');
+
+return(0);
+
+}
