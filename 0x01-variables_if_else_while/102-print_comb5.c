eb1f33c66cf43d60143621943749f62ddf45d4d6
ISHMAEL NYAME <ishmaelnyame7@gmail.com>
2023-06-19 10:00:15 -0700

    COMBO PRINT LAST

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int mynum1;
+int mynum2;
+
+for (mynum1 = 0; mynum1 < 99; mynum1++)
+{
+for (mynum2 = mynum1 + 1; mynum2 < 99; mynum2++)
+{
+putchar(mynum1 /10  + '0');
+putchar ((mynum1 % 10)  +  '0');
+putchar(' ');
+putchar(mynum2 /10  +  '0');
+putchar((mynum2 % 10  +  '0');
+
+if (mynum1 == 99 && mynum2 == 98)
+continue;
+
+putchar(',');
+putchar(' ');
+}
+}
+
+putchar('\n');
+
+return (0);
+
+}
