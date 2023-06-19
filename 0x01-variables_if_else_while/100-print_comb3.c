81fbf02123b099a43aa05218a15cf36eb6dc823d
ISHMAEL NYAME <ishmaelnyame7@gmail.com>
2023-06-19 08:05:27 -0700

    COMB PRINTING 2

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int mynum1;
+int mynum2;
+
+for (mynum1 = 0; mynum1 < 9; mynum1++)
+{
+
+for (mynum2 = mynum1 +1; mynum2 < 10; mynum2++)
+{
+
+putchar((mynum1 % 10) + '0');
+putchar((mynum2 % 10) + '0');
+
+if(mynum1 == 8 && mynum2 == 9)
+{
+continue;
+}
+putchar(' , ');
+putchar('  ');
+}
+}
+putchar('\n');
+
+return (0);
+
+}
