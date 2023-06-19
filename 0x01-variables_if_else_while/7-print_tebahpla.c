1e13628e72d0f54f8da234c8ce5279c8db024552
ISHMAEL NYAME <ishmaelnyame7@gmail.com>
2023-06-19 04:59:32 -0700

    PRINT REVERSE

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char myalphabet;
+
+for (myalphabet = 'z'; myalphabet >= 'a'; myalphabet--)
+{
+putchar(myalphabet);
+}
+putchar('\n');
+
+
+return (0);
+
+}
