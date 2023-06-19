a9aea889650fe54d6799760a190863497d93a754
ISHMAEL NYAME <ishmaelnyame7@gmail.com>
2023-06-19 06:18:28 -0700

    COMB PRINTING

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,14 +13,14 @@ int num;
 for(num = 0;  num < 10;  num++)
 {
 putchar(num%10 + '0');
-}
-if (num == 9)
-{
+
+if (num == 9);
+
 continue;
 putchar(',');
 putchar(' ');
-}
 
+}
 putchar('\n');
 
 return (0);
