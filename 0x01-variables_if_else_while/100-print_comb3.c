764aa3851f6ac5459771481e3442cc8d2952f9f4
ISHMAEL NYAME <ishmaelnyame7@gmail.com>
2023-06-19 08:16:42 -0700

    COMB PRINTING 2

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,13 +14,13 @@ int mynum2;
 for (mynum1 = 0; mynum1 < 9; mynum1++)
 {
 
-for (mynum2 = mynum1 +1; mynum2 < 10; mynum2++)
+for (mynum2 = mynum1 + 1; mynum2 < 10; mynum2++)
 {
 
 putchar((mynum1 % 10) + '0');
 putchar((mynum2 % 10) + '0');
 
-if(mynum1 == 8 && mynum2 == 9)
+if (mynum1 == 8 && mynum2 == 9)
 {
 continue;
 }
