7160cc01b4ebf8ba2d0554bd5c70baba4b352380
ISHMAEL NYAME <ishmaelnyame7@gmail.com>
2023-06-21 13:58:54 -0700

    TASK 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*_abs - Compute the absolute value of an integer
+*@c: The numbrer to be computed
+*Return: for absolute value of 
+zero
+*/
+
+int _abs(int c)
+{
+if (c < 0)
+{
+int abs_val;
+
+abs_val = c * -1;
+
+return (abs_ val);
+
+} 
+
+return (0);
+}
