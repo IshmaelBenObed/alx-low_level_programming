46d44fe71e997f59e26b6489391d27acc4b6299b
ISHMAEL NYAME <ishmaelnyame7@gmail.com>
2023-06-21 06:13:25 -0700

    PRINT AGAIN

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet - make the alphabet x10 times
+ *
+ * Return: void  
+ */
+
+void print_alphabet_x10(void)
+{
+ char i;
+int a = 0;
+
+while (a <= 9)
+{
+for (i = 'a'; a <= 'z'; a++)
+{
+_putchar(i);
+}
+_putchar('\n');
+a++;
+}
+
+putchar ('\n')
+}
