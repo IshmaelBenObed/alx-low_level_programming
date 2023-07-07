2ebd578449528f15b9c62184a960daac70b49ccc
ISHMAEL NYAME <ishmaelnyame7@gmail.com>
2023-07-07 11:36:49 -0700

    CREATING A STATIC LIBRARY

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,5 +1,4 @@
 #include "main.h"
-		
 /**
  * char *_strcpy - a function that copies the string pointed to by src
  * @dest: copy to
@@ -23,5 +22,4 @@ char *_strcpy(char *dest, char *src)
 	dest[l] = '\0';
 	return (dest);
 }
- 
 
