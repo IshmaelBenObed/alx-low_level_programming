9eff34a1f6e8309385e3d6f456328758bac9c1bc
ISHMAEL NYAME <ishmaelnyame7@gmail.com>
2023-07-07 13:44:50 -0700

    ARGUMENT PROJECT

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include "main.h"
-                
 /**
  * main - function thats outputs the string
  * @argc: integer
  * @argv: character
  * Return: 0
  */
+
 int main(int argc, char __attribute__((unused)) *argv[])
 {
 	printf("%d\n", argc - 1);
